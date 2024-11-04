2d05b688a55f169dfeff616c9e41b5fdd3882007
ssnghyxn-k <kimsunghyun8312@gmail.com>
2024-11-04 14:48:51 +0900

    edit boy's self.y

--- boy.py
+++ boy.py
@@ -94,7 +94,7 @@ class Run:
 class Boy:
 
     def __init__(self):
-        self.x, self.y = 400, 90
+        self.x, self.y = 400, 60
         self.face_dir = 1
         self.image = load_image('animation_sheet.png')
         self.state_machine = StateMachine(self)
